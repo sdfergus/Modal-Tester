f74d0c62b6e130bbfe985b9f7954ef1b74ca9536
Shandira <sduxe23@gmail.com>
2021-03-17 21:33:02 -0700

    escape functionality added

--- script.js
+++ script.js
@@ -5,21 +5,31 @@ const overlay = document.querySelector('.overlay');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
+//Refactored open modal code
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//Refactored close modal code
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', function () {
-    // console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  //console.log('close btn clicked');
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+//Event that occurs as soon as you press a key on the keyboard
+document.addEventListener('keydown', function (e) {
+  //   console.log(e);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
 });
