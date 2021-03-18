1290f12ebcb6b155ca06a794728d0b50cf426269
Shandira <sduxe23@gmail.com>
2021-03-17 21:11:09 -0700

    js functionality added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function () {
+    // console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  //console.log('close btn clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
